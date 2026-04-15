f72355455f6864527dde6c1f70b7e5052720f839
kurukurirugveda <kurukurirugved58@gmail.com>
2026-04-15 15:03:58 +0530

    add header and home

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,41 @@
 import "./index.css";
+import { Link } from "react-router";
+import { FaCartShopping } from "react-icons/fa6";
+import { MdOutlineFavoriteBorder } from "react-icons/md";
 const Header = () => {
   return (
     <header>
-      <div className="logo-container">
-        <img src="https://img.pikbest.com/png-images/20241016/creative-book-logo-vector-design_10968791.png!sw800" />
-        <p className="Logo-Text">Bookle</p>
-      </div>
+      <nav>
+        <div className="logo-container">
+          <img src="https://img.pikbest.com/png-images/20241016/creative-book-logo-vector-design_10968791.png!sw800" />
+          <p className="Logo-Text">Booksy</p>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/books" className="nav-link">
+              Shop
+            </Link>
+          </li>
+          <li>
+            <Link to="/cart" className="nav-link">
+              Cart
+            </Link>
+          </li>
+        </ul>
+        <div className="icons-container">
+          <div className="icon-bg">
+            <MdOutlineFavoriteBorder />
+          </div>
+          <div className="icon-bg">
+            <FaCartShopping />
+          </div>
+        </div>
+      </nav>
     </header>
   );
 };
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,6 +1,23 @@
 import Header from "../Header";
-
+import "./index.css";
 const Home = () => {
-  return <Header />;
+  return (
+    <div className="home-bg">
+      <Header />
+      <div className="banner">
+        <div className="banner-text">
+          <h1>Expand Your Mind by reading Booksy Books</h1>
+          <p>
+            Discover a world of knowledge and imagination with our curated
+            collection of books.your digital sanctuary for stories that inspire,
+            educate, and entertain. <br />
+            We believe books are the truest friends, acting as a vast repository
+            of knowledge, imagination, and perspective. One click away. <br />
+          </p>
+          <button>Explore Now</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Home;
