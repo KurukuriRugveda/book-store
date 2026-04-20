12bf5ea0452aca728acc87349ddb2dce940d5346
kurukurirugveda <kurukurirugved58@gmail.com>
2026-04-20 17:34:48 +0530

    add api and fetch

--- src/components/BookList/index.jsx
+++ src/components/BookList/index.jsx
@@ -1,8 +1,20 @@
 import RangeSlider from "react-range-slider-input";
 import Header from "../Header";
 import BookItem from "../BookItem";
+import { useState, useEffect } from "react";
 import "./index.css";
 const BookList = () => {
+  const [bookList, setBookList] = useState([]);
+
+  useEffect(() => {
+    const getBookList = async () => {
+      const apiUrl = "https://www.jsonkeeper.com/b/SQID2";
+      const response = await fetch(apiUrl);
+      const fetchedData = await response.json();
+      setBookList(fetchedData);
+    };
+    getBookList();
+  }, []);
   return (
     <div>
       <Header />
@@ -11,9 +23,10 @@ const BookList = () => {
         placeholder="Search by title, author or genre"
         className="input"
       />
-      <ul>
-        <BookItem />
-        <BookItem />
+      <ul className="book-list">
+        {bookList.map((eachObj) => (
+          <BookItem key={eachObj.uniqueid} bookObj={eachObj} />
+        ))}
       </ul>
     </div>
   );
