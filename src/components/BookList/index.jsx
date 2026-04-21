91cfab818c7275bdcd2e8f099dab9e3a0cbd25a0
kurukurirugveda <kurukurirugved58@gmail.com>
2026-04-21 17:00:08 +0530

    add search functionality and loading screen

--- src/components/BookList/index.jsx
+++ src/components/BookList/index.jsx
@@ -1,33 +1,71 @@
-import RangeSlider from "react-range-slider-input";
+import { Circles } from "react-loader-spinner";
 import Header from "../Header";
 import BookItem from "../BookItem";
 import { useState, useEffect } from "react";
+import { IoSearchSharp } from "react-icons/io5";
 import "./index.css";
 const BookList = () => {
   const [bookList, setBookList] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(true);
+  const [userInput, setUserInput] = useState("");
   useEffect(() => {
     const getBookList = async () => {
       const apiUrl = "https://www.jsonkeeper.com/b/SQID2";
       const response = await fetch(apiUrl);
       const fetchedData = await response.json();
       setBookList(fetchedData);
+      setIsLoading(false);
     };
     getBookList();
   }, []);
-  return (
-    <div>
-      <Header />
-      <input
-        type="search"
-        placeholder="Search by title, author or genre"
-        className="input"
-      />
+  const onChangeSearchInput = (event) => {
+    setUserInput(event.target.value);
+  };
+  const renderLoadingView = () => {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <Circles color="#0077b6" />
+        <h1>Loading</h1>
+      </div>
+    );
+  };
+
+  const renderBooksView = () => {
+    return (
       <ul className="book-list">
-        {bookList.map((eachObj) => (
+        {searchBooks.map((eachObj) => (
           <BookItem key={eachObj.uniqueid} bookObj={eachObj} />
         ))}
       </ul>
+    );
+  };
+  const searchBooks = bookList.filter((eachObj) =>
+    eachObj.Title.toLowerCase().includes(userInput.toLowerCase()),
+  );
+  return (
+    <div>
+      <Header />
+      <div className="search-div">
+        <button className="search-btn">
+          <IoSearchSharp />
+        </button>
+        <input
+          type="search"
+          placeholder="Search by title, author or genre"
+          className="input"
+          value={userInput}
+          onChange={onChangeSearchInput}
+        />
+      </div>
+
+      {isLoading ? renderLoadingView() : renderBooksView()}
     </div>
   );
 };
